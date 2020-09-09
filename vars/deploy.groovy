f996a59f31301b4378ab7a2fc1f3e6c002009ba9
root <root@yuno.net>
2020-09-09 15:25:40 +0000

    bug fix

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,7 +2,7 @@ def call(def server, def port) {
     httpRequest httpMode: 'POST', url: "http://${server}:${port}/shutdown", validResponseCodes: '200,408'
     sshagent(['RemoteCredentials']) {
         sh "scp -o StrictHostKeyChecking=no target/*.jar ec2-user@${server}:~/jenkins-demo.jar"
-        sh "sudo yum install -y java"
+        sh "ssh -o StrictHostKeyChecking=no ec2-user@${server} sudo yum install -y java"
         sh "ssh -o StrictHostKeyChecking=no ec2-user@${server} nohup java -Dserver.port=${port} -jar ~/jenkins-demo.jar &"
     }
     retry (3) {
