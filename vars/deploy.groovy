30522d69e7ea5d3ddb57af21a627343926ac5ac9
root <root@yuno.net>
2020-09-09 15:57:24 +0000

    liferay es efs volume

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,11 +2,10 @@ def call(def server, def port) {
     httpRequest httpMode: 'POST', url: "http://${server}:${port}/shutdown", validResponseCodes: '200,408'
     sshagent(['RemoteCredentials']) {
         sh "scp -o StrictHostKeyChecking=no target/*.jar ec2-user@${server}:~/jenkins-demo.jar"
-        sh "ssh -o StrictHostKeyChecking=no ec2-user@${server} sudo yum install -y java"
         sh "ssh -o StrictHostKeyChecking=no ec2-user@${server} nohup java -Dserver.port=${port} -jar ~/jenkins-demo.jar &"
     }
     retry (3) {
         sleep 5
-        httpRequest url:"http://${server}:${port}", validResponseCodes: '200', validResponseContent: '"status":"UP"'
+        httpRequest url:"http://${server}:${port}", validResponseCodes: '200,404', validResponseContent: '"status":"UP"'
     }
 }
